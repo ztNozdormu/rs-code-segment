ced6c2a9360f067f8d00bb8b4fb457396c895c5d
root <root@localhost.localdomain>
2022-12-09 11:41:47 +0800

    json解析嵌套类型属性为可选类型

--- json-parse/src/main.rs
+++ json-parse/src/main.rs
@@ -19,15 +19,27 @@ fn main() {
         Ok(_) => println!("解析文件内容数据为具体结构对象成功!"),
         Err(err) => println!("解析文件内容数据为具体结构对象发生异常{}", err),
     }
+
+    // 嵌套数据结构
     let test1 = "data/test1.json".to_owned();
     let data_theme = fs::read_to_string(test1).expect("Couldn't find or load that file.");
     let theme = parser::parser_theme(&data_theme);
     match theme {
         Ok(v) => {
-              // 遍历theme数据
-            println!("theme colors Black value is {:#?}",v.colors.get("black"))
-        },
+            // 遍历theme clors 数据
+            for (key, value) in v.colors {
+                println!("theme colors's key is {},value is {}", key, value);
+            }
+            // 可选类型验证
+            println!(
+                "theme breakpoints widescreen(key)'s value is String {:#?}",
+                v.breakpoints.get("widescreen")
+            );
+            println!(
+                "theme breakpoints high(key)'s value is Number {:#?}",
+                v.breakpoints.get("high")
+            );
+        }
         Err(err) => println!("解析文件嵌套内容数据为具体结构对象发生异常{}", err),
     }
-   
 }
--- json-parse/src/parser.rs
+++ json-parse/src/parser.rs
@@ -13,7 +13,7 @@ pub struct Person {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Theme {
     pub animation: HashMap<String, String>,
-    pub breakpoints: HashMap<String, String>,
+    pub breakpoints: HashMap<String, Breakpoint>,
     pub colors: HashMap<String, String>,
     pub fonts: HashMap<String, String>,
     pub font_sizes: Vec<i32>,
@@ -24,6 +24,19 @@ pub struct Theme {
     pub radii: HashMap<String, i32>,
     pub shadows: HashMap<String, HashMap<String, String>>,
     pub gradients: HashMap<String, String>,
+}
+/**
+ * 处理可选类型
+* 但如果我们的属性是可选类型(比如“大小”单位可以是数字10或字符串10px)？在Typescript中，
+ *  我们可以创建这样的类型Size = string | number。在Rust中，这相当于枚举。
+* erde支持Enum类型，如果你传递未标记的宏给他们
+* 比如: Theme的 breakpoints下的数据map,其key对应的value既可能是字符串类型也可能是数字类型
+ */
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum Breakpoint {
+    String(String),
+    Number(i32),
 }
 // example
 pub fn untyped_examples() -> Result<()> {
