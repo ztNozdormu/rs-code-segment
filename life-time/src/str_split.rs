5becbe3590244722d7c3ff6e5b7022da863aa878
root <root@localhost.localdomain>
2022-12-13 17:53:47 +0800

    SplitStr实现new方法

--- life-time/src/str_split.rs
+++ life-time/src/str_split.rs
@@ -1,8 +1,26 @@
 pub struct SplitStr<'a> {
+    #[warn(dead_code)]
     remainder: &'a str,
+    #[warn(dead_code)]
     delimiter: &'a str,
 }
 
 impl<'a> SplitStr<'a> {
-    
+    pub fn new(remainder: &'a str, delimiter: &'a str) -> Self {
+        Self {
+            remainder,
+            delimiter,
+        }
+    }
+}
+
+/**
+ * 实现自定义迭代器
+ */
+impl<'a> Iterator for SplitStr<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
 }
