a3e855e7b7aa60b58938d8d38e10911ba901ac3a
root <root@localhost.localdomain>
2022-12-14 17:48:03 +0800

    迭代测试1

--- life-time/src/main.rs
+++ life-time/src/main.rs
@@ -1,5 +1,7 @@
 pub mod str_split;
-
+use str_split::SplitStr;
 fn main() {
-    println!("Hello, world!");
+    let remainder_str = "a,b,c,d,e,f,g,h,i,j,k,l";
+    let letters: Vec<&str> = SplitStr::new(remainder_str, ",").collect();
+    println!("迭代结果:{:?}", letters)
 }
--- life-time/src/str_split.rs
+++ life-time/src/str_split.rs
@@ -1,8 +1,8 @@
 pub struct SplitStr<'a> {
     #[warn(dead_code)]
-    remainder: &'a str,
+    pub remainder: &'a str,
     #[warn(dead_code)]
-    delimiter: &'a str,
+    pub delimiter: &'a str,
 }
 
 impl<'a> SplitStr<'a> {
@@ -21,6 +21,21 @@ impl<'a> Iterator for SplitStr<'a> {
     type Item = &'a str;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        if let Some(next_index) = self.remainder.find(self.delimiter) {
+            // 截取符合条件的部分
+            let until_remainder = &self.remainder[..next_index];
+            // 剩余部分处理
+            self.remainder = &self.remainder[next_index + self.delimiter.len()..];
+            // 返回符合条件的部分字符
+            Some(until_remainder)
+        } else if self.remainder.is_empty() {
+            //  remainder split完成
+            None
+        } else {
+            // 没有符合分隔条件的字符,清空值并返回
+            let remainder = self.remainder;
+            self.remainder = "";
+            Some(remainder)
+        }
     }
 }
